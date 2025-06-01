17afe0ae2b6e1756641dc4e99cefeeaa0a84a658
crbednarz <cr.bednarz@gmail.com>
2025-06-01 15:20:27 -0700

    Initial test of docker attachment

--- main.go
+++ main.go
@@ -1,113 +1,97 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"io"
-	"log"
 	"os"
-	"os/exec"
 
-	"github.com/creack/pty"
-	"github.com/jroimartin/gocui"
-	"golang.org/x/term"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 )
 
-type TerminalView struct {
-	pty           *os.File
-	oldStdinState term.State
-}
-
-func (t *TerminalView) Layout(view *gocui.View) error {
-	// oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }()
-
-	// go func() { _, _ = io.Copy(t.pty, os.Stdin) }()
-	// _, _ = io.Copy(os.Stdout, t.pty)
-	return nil
-}
-
-func (t *TerminalView) Close() {
-	t.pty.Close()
-	term.Restore(int(os.Stdin.Fd()), &t.oldStdinState)
-}
-
-func NewTerminal(command string) (*TerminalView, error) {
-	c := exec.Command("bash")
-
-	ptmx, err := pty.Start(c)
-	if err != nil {
-		return nil, fmt.Errorf("unable to start terminal: %w", err)
-	}
-
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+func main() {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
-	defer func() { _ = term.Restore(int(os.Stdin.Fd()), oldState) }()
+	defer cli.Close()
 
-	go func() { _, _ = io.Copy(t.pty, os.Stdin) }()
-	_, _ = io.Copy(os.Stdout, t.pty)
-	return &TerminalView{
-		pty: ptmx,
-	}, nil
-}
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", image.PullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+	io.Copy(io.Discard, reader)
 
-type LayoutManager struct {
-	terminal *TerminalView
-}
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image:     "alpine",
+		Cmd:       []string{"/bin/sh"},
+		Tty:       true,
+		OpenStdin: true,
+	}, nil, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+	defer cli.ContainerRemove(ctx, resp.ID, container.RemoveOptions{Force: true})
 
-func NewLayoutManager() *LayoutManager {
-	return &LayoutManager{
-		terminal: nil,
+	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		panic(err)
+	}
+
+	println("Attaching...")
+
+	attachment, err := cli.ContainerAttach(ctx, resp.ID, container.AttachOptions{
+		Stderr: true,
+		Stdout: true,
+		Stdin:  true,
+		Stream: true,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	go io.Copy(os.Stdout, attachment.Reader)
+	go io.Copy(os.Stderr, attachment.Reader)
+
+	inout := make(chan []byte)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			inout <- []byte(scanner.Text())
+		}
+	}()
+
+	// Write to docker container
+	go func(w io.WriteCloser) {
+		for {
+			data, ok := <-inout
+			// log.Println("Received to send to docker", string(data))
+			if !ok {
+				fmt.Println("!ok")
+				w.Close()
+				return
+			}
+
+			w.Write(append(data, '\n'))
+		}
+	}(attachment.Conn)
+
+	err = waitForContainerExit(ctx, cli, resp.ID)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func (l *LayoutManager) SetTerminal(terminal *TerminalView) {
-	l.terminal = terminal
-}
-
-func (l *LayoutManager) Layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", 0, 1, maxX, maxY-2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return fmt.Errorf("unable to set view: %w", err)
-		}
-
-		fmt.Fprintln(v, "Hello world!")
+func waitForContainerExit(ctx context.Context, cli *client.Client, containerId string) error {
+	statusCh, errCh := cli.ContainerWait(ctx, containerId, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		return err
+	case <-statusCh:
 	}
 	return nil
-}
-
-func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicf("error during gui creation: %v", err)
-	}
-	defer g.Close()
-
-	terminal, err := NewTerminal("bash")
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer terminal.Close()
-
-	layout := NewLayoutManager()
-	layout.SetTerminal(terminal)
-
-	g.SetManager(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
 }
