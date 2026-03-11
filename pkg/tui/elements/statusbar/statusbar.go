339cd1079fd9de7db8ba21fae2a31b76a0420419
crbednarz <cr.bednarz@gmail.com>
2026-03-10 18:56:22 -0700

    Improved placeholder status bar to include build status

--- pkg/tui/elements/statusbar/statusbar.go
+++ pkg/tui/elements/statusbar/statusbar.go
@@ -1,13 +1,24 @@
 package statusbar
 
 import (
+	"fmt"
+
 	tea "charm.land/bubbletea/v2"
 	"github.com/crbednarz/df-explorer/pkg/explorer"
 	"github.com/crbednarz/df-explorer/pkg/tui/style"
 )
 
+type BuildStatus int
+
+const (
+	StatusPending BuildStatus = iota
+	StatusInProgress
+	StatusCompleted
+)
+
 type Model struct {
-	name string
+	name   string
+	status BuildStatus
 }
 
 func New(theme *style.Theme) *Model {
@@ -22,11 +33,45 @@ func (m *Model) Update(msg tea.Msg) tea.Cmd {
 	switch msg := msg.(type) {
 	case explorer.DockerfileEvent:
 		m.name = msg.Dockerfile.FileName()
+	case explorer.BuildStartEvent:
+		m.status = StatusPending
+	case explorer.BuildProgressEvent:
+		m.handleProgress(msg)
+	case explorer.BuildEndEvent:
+		m.status = StatusCompleted
 	}
 
 	return nil
 }
 
 func (m *Model) View() string {
-	return m.name
+	var status string
+	switch m.status {
+	case StatusCompleted:
+		status = "Completed"
+	case StatusInProgress:
+		status = "In Progress"
+	case StatusPending:
+		status = "Pending"
+	}
+	return fmt.Sprintf("%s %s", m.name, status)
+}
+
+func (m *Model) handleProgress(event explorer.BuildProgressEvent) {
+	status := event.Status
+	total := 0
+	completed := 0
+
+	for _, s := range status.Statuses {
+		total++
+		if s.Completed != nil {
+			completed++
+		}
+	}
+
+	if total == completed {
+		m.status = StatusCompleted
+	} else {
+		m.status = StatusInProgress
+	}
 }
